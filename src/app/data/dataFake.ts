a15b49a0d1cf14122237cc7696baf5758f6af834
dsenna <davidsenna.if@gmail.com>
2023-05-02 08:44:41 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake =[
+    {
+        "id":"1",
+        "title": "NOVO HOMEM DE FERRO ANUNCIADO",
+        "description": "Marvel studios anuncia novo filme do homem de ferro.",
+        "photoCover": "https://cdn.britannica.com/49/182849-050-4C7FE34F/scene-Iron-Man.jpg"
+    },
+    {
+        "id":"2",
+        "title": "Nova série anuncia",
+        "description": "bla blabla",
+        "photoCover": "https://nosbastidores.com.br/wp-content/uploads/2021/08/Series-Marvel-Disney-Plus-1024x576-1.jpg"
+    },
+]
